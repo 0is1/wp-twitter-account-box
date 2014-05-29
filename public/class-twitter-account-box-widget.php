b1d2d170d1902ba6f96d41e76a74f679b8d16c71
Janne Saarela <janne.juhani@gmail.com>
2014-05-29 21:09:55 +0300

    Add initial widget implementation

---
+++ public/class-twitter-account-box-widget.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+*
+* @package        Twitter Account Box
+* @author         Janne Saarela <janne@jannejuhani.net>
+* @license        GPL-2.0+
+* @link           http://tab.jannejuhani.net
+* @copyright      2014 Janne Saarela
+*/
+
+class TwitterAccountBoxWidget extends WP_Widget {
+  function __construct() {
+    parent::__construct(
+      'twitteraccountbox_widget',
+      __( 'Twitter Account Box Widget' , 'twitteraccountbox'),
+      array( 'description' => __( 'Display your Twitter account box' , 'twitteraccountbox') )
+    );
+  }
+
+  /**
+   * Outputs the options form on admin
+   *
+   * @param array $instance The widget options
+   */
+  public function form( $instance ) {
+    if ( $instance ) {
+      $title = $instance['title'];
+    }
+    else {
+      $title = __( 'Twitter' , 'twitteraccountbox');
+    }
+?>
+
+    <p>
+    <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title:' , 'twitteraccountbox'); ?></label>
+    <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+    </p>
+
+<?php
+  }
+  /**
+   * Processing widget options on save
+   *
+   * @param array $new_instance The new options
+   * @param array $old_instance The previous options
+   */
+  public function update( $new_instance, $old_instance ) {
+    $instance['title'] = strip_tags( $new_instance['title'] );
+    return $instance;
+  }
+  /**
+   * Outputs the content of the widget
+   *
+   * @param array $args
+   * @param array $instance
+   */
+  public function widget( $args, $instance ) {
+    // $count = get_option( 'akismet_spam_count' );
+
+    echo $args['before_widget'];
+    if ( ! empty( $instance['title'] ) ) {
+      echo $args['before_title'];
+      echo esc_html( $instance['title'] );
+      echo $args['after_title'];
+    }
+?>
+
+  <div class="a-stats">
+    <h1>MOI!</h1>
+  </div>
+
+<?php
+    echo $args['after_widget'];
+  }
+}
+
+function tab_register_widgets() {
+  register_widget( 'TwitterAccountBoxWidget' );
+}
+
+add_action( 'widgets_init', 'tab_register_widgets' );
